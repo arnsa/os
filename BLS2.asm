15787ee6a73af7f509157f4ac1937cb5cf1254d9
Arnas <giant1gf@gmail.com>
2013-01-28 01:10:13 +0200

    Added elf parsing

--- BLS2.asm
+++ BLS2.asm
@@ -1,9 +1,11 @@
+
 bits 16
 
 org 0x8000
 
 jmp start
 
+offset:    dd 0
 Good db "All good yo!", 0
 
 Print:
@@ -76,6 +78,32 @@ a20:
     mov ss, ax
     mov es, ax
     mov esp, 0x90000
+    mov al, 0x02
+    out 0x92, al
 
-	mov al, 0xDD
-	out 0x64, al
+boot_elf:
+    mov eax, dword[eof+0x1C]
+    mov [offset], eax
+    movzx ecx, word[eof+0x2C]
+    jmp near load_segments
+
+load_segments:
+    push ecx
+    mov eax, dword[offset]
+    add eax, 0x04
+    mov esi, dword[eof+eax] ; move p_offset
+    add esi, eof
+    add eax, 0x04
+    mov edi, dword[eof+eax] ; move p_vaddr
+    add eax, 0x08
+    mov ecx, dword[eof+eax] ; move p_filesz file starts at p_offset, should be loaded at p_vaddr and it's size is p_memsz
+    rep movsb
+    pop ecx
+    mov eax, [offset]
+    add eax, [eof+0x2A]
+    mov [offset], eax
+    dec ecx
+    jnz load_segments
+    jmp dword[eof+0x18]
+
+eof:
